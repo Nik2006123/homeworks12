914c4716d88223f34a2d789854aba14afd0066be
Nik2006123 <141873498+Nik2006123@users.noreply.github.com>
2023-08-15 04:07:06 -0400

    Add files via upload

---
+++ homwork/homwork.py
@@ -0,0 +1,13 @@
+for i in range(1,100):
+    if i % 3 == 0:
+     print("GOA")
+
+for i in range(1,100):
+   if i % 5 == 0:
+          print("GOA11")
+
+for i in range(1,100):
+    if i % 15 == 0:
+            print("GOA15")
+
+
